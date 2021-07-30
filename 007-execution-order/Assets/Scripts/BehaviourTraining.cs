b623c3c011dc9b648be9065628b4bcb80de4df46
Ikram-Maulana <ikram_maulana@onedrive.web.id>
2021-07-30 14:38:45 +0700

    update method. moving a object
    mengubah method update supaya bisa memindahkan objek

--- 007-execution-order/Assets/Scripts/BehaviourTraining.cs
+++ 007-execution-order/Assets/Scripts/BehaviourTraining.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BehaviourTraining : MonoBehaviour
-{ 
+{
     // Use this method before initialization
     private void Awake()
     {
@@ -19,6 +19,14 @@ public class BehaviourTraining : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.W))
+            transform.position = transform.position + Vector3.forward;
+        if (Input.GetKeyDown(KeyCode.S))
+            transform.position = transform.position + Vector3.back;
+        if (Input.GetKeyDown(KeyCode.A))
+            transform.position = transform.position + Vector3.left;
+        if (Input.GetKeyDown(KeyCode.D))
+            transform.position = transform.position + Vector3.right;
         Debug.Log(Time.time);
     }
 }
