10d0b2bb8b6c4cd85fc439192d2fa5122213003d
Ikram-Maulana <ikram_maulana@onedrive.web.id>
2021-07-30 15:33:31 +0700

    decision making not statement and operator

--- 008 - Decision Making/Assets/Scripts/Rain.cs
+++ 008 - Decision Making/Assets/Scripts/Rain.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Rain : MonoBehaviour
 {
     public bool isItGoingToRain = true;
+    public bool amIanEmployee = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Mengubah value amIanEmployee
+        amIanEmployee = !amIanEmployee;
+
         // if true
         if (isItGoingToRain)
         {
@@ -19,6 +23,14 @@ public class Rain : MonoBehaviour
         {
             Debug.Log("I don't need to take my umbrella");
             Debug.Log("The value of isItGoingToRain is = " + isItGoingToRain);
+        }
+
+        if (!amIanEmployee)
+        {
+            Debug.Log("No, you are not an employee");
+        } else
+        {
+            Debug.Log("Yes, you are an employee");
         }
             
     }
